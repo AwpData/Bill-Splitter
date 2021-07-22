ee03694fba64675c9c58976f6ab1bd65c81e33f9
AwpData <awpdata@gmail.com>
2021-07-22 14:48:00 -0700

    Final program

--- Bill_Splitter.py
+++ Bill_Splitter.py
@@ -1,3 +1,5 @@
+import random
+
 print("Enter the number of friend joining (including you)")
 friend = int(input())
 friends = dict()
@@ -7,8 +9,23 @@ else:
     print("Enter the name of every friend (including you), each on a new line:")
     for i in range(friend):
         friends[str(input())] = 0
+
     print("Enter the total bill value:")
     total = int(input())
-    split_value = round(total / len(friends), 2)
-    friends = dict.fromkeys(friends, split_value)
+
+    print("Do you want to use the \"Who is lucky?\" feature? Write Yes/No")
+    choice = str(input())
+    if choice == "Yes":
+        lucky_person = random.choice(list(friends.keys()))
+        print("{} is the lucky one!".format(lucky_person))
+        split_value = round(total / (len(friends) - 1), 2)
+        for key in friends:
+            if key == lucky_person:
+                friends[key] = 0
+            else:
+                friends[key] = split_value
+    else:
+        print("No one is going to be lucky")
+        split_value = round(total / len(friends), 2)
+        friends = dict.fromkeys(friends, split_value)
     print(friends)
