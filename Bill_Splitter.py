4ad3ed96ba7ccc58ca79aad6d02ba21320a587d6
AwpData <awpdata@gmail.com>
2021-07-22 14:20:35 -0700

    Added more code

--- Bill_Splitter.py
+++ Bill_Splitter.py
@@ -1,11 +1,14 @@
 print("Enter the number of friend joining (including you)")
 friend = int(input())
+friends = dict()
 if friend <= 0:
     print("No one is joining for the party")
 else:
-    friends = dict()
     print("Enter the name of every friend (including you), each on a new line:")
     for i in range(friend):
         friends[str(input())] = 0
-
+    print("Enter the total bill value:")
+    total = int(input())
+    split_value = round(total / len(friends), 2)
+    friends = dict.fromkeys(friends, split_value)
     print(friends)
